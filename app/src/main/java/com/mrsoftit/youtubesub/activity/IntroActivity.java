5ca86620e15d771fc8e093dfc96da5ef8452269c
mijan <mijantck3.ofic@gmail.com>
2021-07-18 03:35:32 +0600

    Intro and Splash screen add

--- app/src/main/java/com/mrsoftit/youtubesub/activity/IntroActivity.java
+++ app/src/main/java/com/mrsoftit/youtubesub/activity/IntroActivity.java
@@ -1,16 +1,60 @@
 package com.mrsoftit.youtubesub.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.github.appintro.AppIntro;
+import com.github.appintro.AppIntroFragment;
 import com.mrsoftit.youtubesub.R;
 
-public class IntroActivity extends AppCompatActivity {
+import static com.mrsoftit.youtubesub.activity.SplashActivity.MY_PREFS_NAME;
+
+public class IntroActivity extends AppIntro {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        addSlide(AppIntroFragment.newInstance("Videos View", "Video View  from anywhere and\n" +
+                        "anytime learn at your own pace.",
+                R.drawable.into_one, ContextCompat.getColor(getApplicationContext(), R.color.intro_color)));
+
+        // below line is for creating second slide.
+        addSlide(AppIntroFragment.newInstance("Earn money ", "Get money  based on your \n" +
+                        "coin collection ",
+                R.drawable.intro_two, ContextCompat.getColor(getApplicationContext(), R.color.intro_color)));
+
+        // below line is use to create third slide.
+        addSlide(AppIntroFragment.newInstance("Like Video  ", "Take the best online youtube\n" +
+                        " like promotion app.",
+                R.drawable.intro_tre, ContextCompat.getColor(getApplicationContext(), R.color.intro_color)));
+
+        addSlide(AppIntroFragment.newInstance("Subscriber", "subscribe from trustet  subscriber",
+                R.drawable.intreo_four, ContextCompat.getColor(getApplicationContext(), R.color.intro_color)));
+    }
+
+    @Override
+    protected void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("intro", "ok");
+        editor.apply();
+        Intent i = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("intro", "ok");
+        editor.apply();
+        Intent i = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/mrsoftit/youtubesub/activity/SplashActivity.java
+++ app/src/main/java/com/mrsoftit/youtubesub/activity/SplashActivity.java
@@ -2,15 +2,48 @@ package com.mrsoftit.youtubesub.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import com.mrsoftit.youtubesub.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        String inro = prefs.getString("intro", "No name defined");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i;
+                if (!inro.equals("ok")){
+                    i = new Intent(SplashActivity.this, IntroActivity.class);
+                }else {
+                    i = new Intent(SplashActivity.this, MainActivity.class);
+                }
+                //Intent is used to switch from one activity to another.
+                startActivity(i);
+                //invoke the SecondActivity.
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
